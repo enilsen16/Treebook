26df005bec9a0b8b2d30aec817a2bffb441e3101
Erik Nilsen <enilsen16@live.com>
2014-02-24 15:55:19 -0800

    Created a new app called Treebook; installed Twitter Bootstrap; and configured some front end.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Treebook::Application.config.secret_key_base = '8bbd35ec400c418c4f8998853c4793aaac7f1c69a6ab6c2c1be3bbefb52efd8d9e0a5ed7d6b797e458be85663aceb6ba690104e7983f935d5e102259668a2e02'
